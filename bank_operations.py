32978aac9df537337610ca909f1bf568c987c4f9
ATApostolov21 <ATApostolov21@codingburgas.bg>
2024-01-10 23:50:20 +0200

    Add create account function and arranged the files in their places

--- bank_operations.py
+++ bank_operations.py
@@ -1,16 +1,8 @@
 # bank_operations.py
 
-from user_authentication import get_user_by_names, save_card_holders_to_json
+from user_authentication import get_user_by_names
+from user_authentication import  save_card_holders_to_json
 
-def print_menu():
-    print("Please choose from one of the following options...")
-    print("1. Deposit")
-    print("2. Withdrawal")
-    print("3. Show Balance")
-    print("4. Transfer")
-    print("5. Change User Information")
-    print("6. Close Account")
-    print("7. Exit")
 
 def deposit(card_holder, list_of_card_holders):
     try:
@@ -24,6 +16,7 @@ def deposit(card_holder, list_of_card_holders):
 
     except ValueError:
         print("Invalid input")
+
 
 def withdrawal(card_holder, list_of_card_holders):
     try:
@@ -41,7 +34,8 @@ def withdrawal(card_holder, list_of_card_holders):
     except ValueError:
         print("Invalid input")
 
-def check_balance(card_holder):
+
+def check_balance_and_transaction_history(card_holder):
     print("Your current balance is: ", card_holder.get_balance())
     print("Deposit History: ")
     for transaction in card_holder.get_transaction_history():
@@ -51,48 +45,6 @@ def check_balance(card_holder):
     for transaction in card_holder.get_transaction_history():
         if transaction[0] == "Withdrawal":
             print(transaction)
-
-
-def change_user_info(current_user, list_of_card_holders):
-    print("Select what you want to change:")
-    print("1. First Name")
-    print("2. Last Name")
-    print("3. PIN")
-
-    try:
-        choice = int(input("Enter your choice (1-3): ").strip())
-
-        if choice == 1:
-            new_value = input("Enter your new first name: ").strip()  # Limit to 10 characters
-            if len(new_value) > 10:
-                print("Invalid name length. Maximum length is 10 characters. Please try again.")
-                return
-            current_user.set_firstName(new_value)
-        elif choice == 2:
-            new_value = input("Enter your new last name: ").strip()  # Limit to 10 characters
-            if len(new_value) > 10:
-                print("Invalid name length. Maximum length is 10 characters. Please try again.")
-                return
-            current_user.set_lastName(new_value)
-        elif choice == 3:
-            new_value = int(input("Enter your new PIN: ").strip())
-            # Check if the new PIN is exactly 4 digits
-            if 1000 <= new_value <= 9999:
-                current_user.set_pin(new_value)
-            else:
-                print("Invalid PIN. Please enter a 4-digit PIN.")
-                return
-        else:
-            print("Invalid choice. Please enter a number between 1 and 3.")
-            return
-
-        print("User information changed successfully!")
-
-        # Save updated data to JSON file after changing user info
-        save_card_holders_to_json(list_of_card_holders, 'card_holders.json')
-
-    except ValueError:
-        print("Invalid input. Please enter valid information.")
 
 
 def transfer(current_user, list_of_card_holders):
@@ -145,82 +97,11 @@ def transfer(current_user, list_of_card_holders):
     # Save updated data to JSON file when modifying something
     save_card_holders_to_json(list_of_card_holders, 'card_holders.json')
 
-def close_account(current_user, card_holders_list):
-    print("Closing your account...")
-
-    # Ask the user how they want to handle the remaining balance
-    print("Choose an option:")
-    print("1. Withdraw the remaining balance")
-    print("2. Transfer the remaining balance to another account")
-
-    try:
-        choice = int(input("Enter your choice (1-2): ").strip())
-
-        if choice == 1:
-            # Automatically withdraw all of the remaining balance
-
-            print("Warning: Choosing to withdraw will remove all of your money from your account.")
-            confirm = input("Are you sure you want to proceed? (yes/no): ").strip().lower()
-
-            if confirm == "yes":
-                withdrawal_amount = current_user.get_balance()
-                current_user.set_balance(0)
-                current_user.add_to_history("Account Closure - Withdrawal", withdrawal_amount)
-                print(f"Withdrawn {withdrawal_amount} from your account. Account closed successfully.")
-            else:
-                print("Withdraw canceled. Account closure canceled.")
-                return
-
-        elif choice == 2:
-            print("Warning: Choosing to transfer will remove all of your money from your account.")
-            confirm = input("Are you sure you want to proceed? (yes/no): ").strip().lower()
-
-            if confirm == "yes":
-                # Call the transfer_for_closing_account function to handle the transfer
-                transfer_for_closing_account(current_user, card_holders_list)
-            else:
-                print("Transfer canceled. Account closure canceled.")
-                return
-        else:
-            print("Invalid choice. Account closure canceled.")
-            return
-
-        # Remove the user from the list of cardholders
-        card_holders_list.remove(current_user)
-
-        # Save the updated list to the JSON file
-        save_card_holders_to_json(card_holders_list, 'card_holders.json')
-
-    except ValueError:
-        print("Invalid input. Please enter a valid choice.")
 
 
-def transfer_for_closing_account(sender, list_of_card_holders):
-    print("Enter receiver's information:")
-    receiver_first_name = input("Receiver's first name: ")
-    receiver_last_name = input("Receiver's last name: ")
-    receiver_card_num = input("Receiver's card number: ")
 
-    receiver = get_user_by_names(receiver_first_name, receiver_last_name, receiver_card_num, list_of_card_holders)
 
-    if receiver is None:
-        print(f"Receiver {receiver_first_name} {receiver_last_name} not found in the list of card holders.")
-        return
 
-    # Transfer all of the sender's money to the receiver
-    amount = sender.get_balance()
-    sender.set_balance(0)
-
-    sender.add_to_history(f"Transfer to {receiver.get_firstName()} {receiver.get_lastName()}", amount)
-    receiver_balance = receiver.get_balance()
-    receiver.set_balance(receiver_balance + amount)
-    receiver.add_to_history(f"Received from {sender.get_firstName()} {sender.get_lastName()}", amount)
-
-    print("Transfer successful!")
-    print(f"Sender's balance: {sender.get_balance()}")
-    print(f"Receiver's balance: {receiver.get_balance()}")
-
-    save_card_holders_to_json(list_of_card_holders, 'card_holders.json')
 
 
 
--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # main.py
-
-from bank_operations import print_menu, deposit, withdrawal, check_balance, transfer, change_user_info, close_account
+from menu import print_menu
+from account_operations import create_account, close_account, change_user_info
 from user_authentication import authenticate_user, get_user_pin, load_card_holders_from_json
+from bank_operations import deposit, withdrawal, transfer, check_balance_and_transaction_history
 
 def main():
     list_of_card_holders = load_card_holders_from_json('card_holders.json')
 
-    current_user = authenticate_user(list_of_card_holders)
+    # Ask the user if they have an account or want to create a new one
+    while True:
+        print("Do you have an account?")
+        print("1. Yes")
+        print("2. No, create a new account")
+
+        try:
+            choice = int(input("Enter your choice (1-2): ").strip())
+            if choice == 1:
+                current_user = authenticate_user(list_of_card_holders)
+                break
+            elif choice == 2:
+                create_account(list_of_card_holders)
+                current_user = authenticate_user(list_of_card_holders)
+                break
+            else:
+                print("Invalid choice. Please enter 1 or 2.")
+        except ValueError:
+            print("Invalid input. Please enter a valid choice.")
 
     if get_user_pin(current_user):
         print("Welcome ", current_user.get_firstName(), " :)")
@@ -24,7 +43,7 @@ def main():
         elif option == 2:
             withdrawal(current_user, list_of_card_holders)
         elif option == 3:
-            check_balance(current_user)
+            check_balance_and_transaction_history(current_user)
         elif option == 4:
             transfer(current_user, list_of_card_holders)
         elif option == 5:
@@ -39,7 +58,7 @@ def main():
         else:
             print("Invalid option. Please try again.")
 
-        print("Have a nice day :)")
+    print("Have a nice day :)")
 
 if __name__ == "__main__":
     main()
