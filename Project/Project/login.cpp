dacc1b766d025e00eb877e2b8ea7e82b81b34de0
GYFilipov21 <GYFilipov21@codingburgas.bg>
2024-01-20 14:40:22 +0200

    Make the login input a function

--- Project/Project/login.cpp
+++ Project/Project/login.cpp
@@ -1,5 +1,12 @@
 #include "Header.h"
 #define MAX_INPUT_CHARS 24
+Rectangle emailInputBox = { 15, loginHeight / 2 - 100, 275, 30 };
+Rectangle passwordInputBox = { 15, loginHeight / 2 - 25, 275, 30 };
+bool isTypingGlobal[2] = { 0 , 0 };
+int framesCounter = 0;
+int backspaceCounter = 0;
+int backspaceHoldDown = 0;
+int charCountGlobal[2] = { 0 , 0 };
 
 struct LOGIN_INFO 
 {
@@ -13,6 +20,78 @@ string convertToString(char* arr)
     return str;
 }
 
+bool getInput(Rectangle inputBox, bool isTyping, char inputArr[], int charCount)
+{
+
+    if (CheckCollisionPointRec(GetMousePosition(), inputBox))
+    {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && isTyping == 0)
+        {
+            isTyping = 1;
+        }
+    }
+
+    else if (isTyping == 1 && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        isTyping = 0;
+    }
+
+    if (CheckCollisionPointRec(GetMousePosition(), inputBox)) SetMouseCursor(MOUSE_CURSOR_IBEAM);
+    if (isTyping == 1)
+    {
+
+        // Get char pressed (unicode character) on the queue
+        int key = GetCharPressed();
+
+        // Check if more characters have been pressed on the same frame
+        while (key > 0)
+        {
+            if ((key >= 32) && (key <= 125) && (charCount < MAX_INPUT_CHARS))
+            {
+                inputArr[charCount] = (char)key;
+                inputArr[charCount + 1] = '\0'; // Add null terminator at the end of the string.
+                charCount++;
+            }
+
+            key = GetCharPressed();  // Check next character in the queue
+        }
+        if (IsKeyDown(KEY_BACKSPACE))
+        {
+            if (backspaceCounter > 30)
+            {
+                backspaceHoldDown++;
+                if (backspaceHoldDown > 3)
+                {
+                    charCount--;
+                    if (charCount < 0) charCount = 0;
+                    inputArr[charCount] = '\0';
+                    backspaceHoldDown = 0;
+                }
+
+            }
+            backspaceCounter++;
+        }
+
+
+        if (IsKeyPressed(KEY_BACKSPACE))
+        {
+            charCount--;
+            if (charCount < 0) charCount = 0;
+            inputArr[charCount] = '\0';
+        }
+    }
+  
+    if (isTyping == 1) framesCounter++;
+    
+    if (isTypingGlobal[0] == 1) {
+        charCountGlobal[0] = charCount;
+    }
+    if (isTypingGlobal[1] == 1) {
+        charCountGlobal[1] = charCount;
+    }
+    return isTyping;
+}
+
 bool login()
 {
     InitWindow(loginWidth, loginHeight, "PATENT BANK | PLEASE LOG IN . . .");
@@ -23,16 +102,8 @@ bool login()
     // Sample login info
 
     LOGIN_INFO accounts[10];
-    accounts[0].email = "john.smith@gmail.com";
+    accounts[0].email = "john@gmail.com";
     accounts[0].password = "123456";
-
-    accounts[1].email = "mag.papazov@abv.com";
-    accounts[1].password = "AudiA8";
-
-    accounts[2].email = "admin@patent.mail";
-    accounts[2].password = "admin123";
-
-    bool isTyping[2] = { 0 , 0 };
 
     // EMAIL INPUT BOX
     char emailInput[MAX_INPUT_CHARS + 1] = "\0";
@@ -42,158 +113,23 @@ bool login()
     char passwordInput[MAX_INPUT_CHARS + 1] = "\0";
     char passwordInputHidden[MAX_INPUT_CHARS + 1] = "\0";
     int passwordCharCount = 0;
-
-
     int backspaceHoldDown = 0;
-
-    Rectangle emailInputBox = { 15, loginHeight / 2 - 100, 275, 30 };
-    Rectangle passwordInputBox = { 15, loginHeight / 2 - 25, 275, 30 };
+    
 
 
-    int backspaceCounter = 0;
-    int emailFramesCounter = 0;
-    int passwordFramesCounter = 0;
+
    
     DrawTexture(logo, 200, screenHeight / 2, WHITE);
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         SetMouseCursor(MOUSE_CURSOR_DEFAULT);
-        ///////////////////// EMAIL INPUT /////////////////////
-        if (CheckCollisionPointRec(GetMousePosition(), emailInputBox))
-        {  
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && isTyping[0] == 0)
-            {
-                isTyping[0] = 1;
-            }
-        }
-
-        else if (isTyping[0] == 1 && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            isTyping[0] = 0;
-        }
-
-        if(CheckCollisionPointRec(GetMousePosition(), emailInputBox)) SetMouseCursor(MOUSE_CURSOR_IBEAM);
-        if (isTyping[0] == 1)
-        {        
-
-            // Get char pressed (unicode character) on the queue
-            int key = GetCharPressed();
-
-            // Check if more characters have been pressed on the same frame
-            while (key > 0)
-            {
-                if ((key >= 32) && (key <= 125) && (emailCharCount < MAX_INPUT_CHARS))
-                {
-                    emailInput[emailCharCount] = (char)key;
-                    emailInput[emailCharCount + 1] = '\0'; // Add null terminator at the end of the string.
-                    emailCharCount++;
-                }
-
-                key = GetCharPressed();  // Check next character in the queue
-            }
-            if (IsKeyDown(KEY_BACKSPACE))
-            {
-                if (backspaceCounter > 30)
-                {
-                    backspaceHoldDown++;
-                    if (backspaceHoldDown > 3)
-                    {
-                        emailCharCount--;
-                        if (emailCharCount < 0) emailCharCount = 0;
-                        emailInput[emailCharCount] = '\0';
-                        backspaceHoldDown = 0;
-                    }
-
-                }
-                backspaceCounter++;
-            }
-
-
-            if (IsKeyPressed(KEY_BACKSPACE))
-            {
-                emailCharCount--;
-                if (emailCharCount < 0) emailCharCount = 0;
-                emailInput[emailCharCount] = '\0';
-            }
-        }
-        if (isTyping[0] == 1) emailFramesCounter++;
-        else emailFramesCounter = 0;
-        ///////////////////////////////////////////////////////
-
-
-
-        ///////////////////// PASSWORD INPUT //////////////////
-        if (CheckCollisionPointRec(GetMousePosition(), passwordInputBox))
-        {
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && isTyping[1] == 0)
-            {
-                isTyping[1] = 1;
-            }
-        }
-
-        else if (isTyping[1] == 1 && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            isTyping[1] = 0;
-        }
-
-        if (CheckCollisionPointRec(GetMousePosition(), passwordInputBox)) SetMouseCursor(MOUSE_CURSOR_IBEAM);
-            
-        if (isTyping[1] == 1)
-        {
-
-            // Get char pressed (unicode character) on the queue
-            int key = GetCharPressed();
-
-            // Check if more characters have been pressed on the same frame
-            while (key > 0)
-            {
-                if ((key >= 32) && (key <= 125) && (passwordCharCount < MAX_INPUT_CHARS))
-                {
-                    passwordInput[passwordCharCount] = (char)key;
-                    passwordInputHidden[passwordCharCount] = '*';
-                    passwordInput[passwordCharCount + 1] = '\0'; // Add null terminator at the end of the string.
-                    passwordCharCount++;
-                }
-
-                key = GetCharPressed();  // Check next character in the queue
-            }
-            if (IsKeyDown(KEY_BACKSPACE))
-            {
-                if (backspaceCounter > 30)
-                {
-                    backspaceHoldDown++;
-                    if (backspaceHoldDown > 3)
-                    {
-                        passwordCharCount--;
-                        if (passwordCharCount < 0) passwordCharCount = 0;
-                        passwordInput[passwordCharCount] = '\0';
-                        passwordInputHidden[passwordCharCount] = '\0';
-                        backspaceHoldDown = 0;
-                    }
-
-                }
-                backspaceCounter++;
-            }
-
-
-            if (IsKeyPressed(KEY_BACKSPACE))
-            {
-                passwordCharCount--;
-                if (passwordCharCount < 0) passwordCharCount = 0;
-                passwordInput[passwordCharCount] = '\0';
-                passwordInputHidden[passwordCharCount] = '\0';
-            }
-
-        }
-        if (isTyping[1] == 1) passwordFramesCounter++;
-        else passwordFramesCounter = 0;
-        ///////////////////////////////////////////////////////
-
         if (!(IsKeyDown(KEY_BACKSPACE))) {
             backspaceHoldDown = 0;
             backspaceCounter = 0;
         }
+
+
         BeginDrawing();
 
         Color backgroundColor = { 225, 225, 225, 255};
@@ -211,20 +147,17 @@ bool login()
         ///////////////////// EMAIL INPUT /////////////////////
         DrawTextEx(customFont, "Email:", Vector2{ (float)15, (float)loginHeight / 2 - 125 }, 20, 2, BLACK);
         DrawRectangle(15, loginHeight / 2 - 100, 275, 30, WHITE);
-
-        if (isTyping[0] == 1) DrawRectangleLines((int)emailInputBox.x, (int)emailInputBox.y, (int)emailInputBox.width, (int)emailInputBox.height, BLACK);
-        else DrawRectangleLines((int)emailInputBox.x, (int)emailInputBox.y, (int)emailInputBox.width, (int)emailInputBox.height, DARKGRAY);
-
-        DrawTextEx(customFont, emailInput, Vector2{ (float)emailInputBox.x + 5, (float)emailInputBox.y + 4 }, 20, 2, BLACK);
-
-        if (isTyping[0] == 1)
-        {
-            if (emailCharCount < MAX_INPUT_CHARS)
-            {
-                // Draw blinking char
-                if (((emailFramesCounter / 20) % 2) == 0) DrawText("|", (int)emailInputBox.x + 8 + MeasureText(emailInput, 20), (int)emailInputBox.y + 6, 20, BLACK);
-            }
+        if (CheckCollisionPointRec(GetMousePosition(), emailInputBox) || isTypingGlobal[0] == 1) {
+            isTypingGlobal[0] = getInput(emailInputBox, isTypingGlobal[0], emailInput, charCountGlobal[0]);
         }
+        DrawRectangleLines((int)emailInputBox.x, (int)emailInputBox.y, (int)emailInputBox.width, (int)emailInputBox.height, DARKGRAY);
+        DrawTextEx(customFont, emailInput, Vector2{ (float)emailInputBox.x + 5, (float)emailInputBox.y + 4 }, 20, 2, BLACK);
+        if (isTypingGlobal[0] == 1)
+        {
+            if (((framesCounter / 20) % 2) == 0) DrawText("|", (int)emailInputBox.x + 8 + MeasureText(emailInput, 20), (int)emailInputBox.y + 6, 20, BLACK);
+ 
+        }
+        
         ///////////////////////////////////////////////////////
         DrawTexture(logo, 200, screenHeight / 2, BLACK);
 
@@ -237,16 +170,21 @@ bool login()
 
         DrawText(passwordInputHidden, (int)passwordInputBox.x + 5, (int)passwordInputBox.y + 4, 20, BLACK);
 
-        if (isTyping[1] == 1)
-        {
-            if (passwordCharCount < MAX_INPUT_CHARS)
-            {
-                // Draw blinking char
-                if (((passwordFramesCounter / 20) % 2) == 0) DrawText("|", (int)passwordInputBox.x + 8 + MeasureText(passwordInputHidden, 20), (int)passwordInputBox.y + 6, 20, BLACK);
-            }
-        }
+        
 
         ///////////////////////////////////////////////////////
+        if ((isTypingGlobal[0] == 0 && isTypingGlobal[1] == 0) || (framesCounter > 40))
+        {
+            framesCounter = 0;
+        }
+        if (isTypingGlobal[0] == 0)
+        {
+            DrawTextEx(customFont, "Password:", Vector2{ (float)15, (float)loginHeight / 2 - 50 }, 20, 2, RED);
+        }
+      
+
+                DrawTextEx(customFont, ("Frame time: " + to_string(framesCounter)).c_str(), Vector2{ (float)155, (float)loginHeight / 2 - 125 }, 20, 2, BLACK);
+
 
         // LOGIN BUTTON
         DrawRectangle(15, loginHeight / 2 + 50, 275, 40, loginButtonColor);
